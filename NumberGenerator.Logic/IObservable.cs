acb66f1054e918058ab2edd79d4cec44b7dd7c33
zvonkokolev <zvonko.kolev@liwest.at>
2019-12-21 20:06:10 +0100

    Projektdateien hinzufügen.

---
+++ NumberGenerator.Logic/IObservable.cs
@@ -0,0 +1,8 @@
+﻿namespace NumberGenerator.Logic
+{
+	public interface IObservable
+	{
+		public delegate void NextNumberHandler(int number);
+		public NextNumberHandler NextNumberHandlerChanged { get; set; }
+	}
+}
